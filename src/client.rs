ddb796c5ed9f0bbf91f289722d9370ffe8b327ac
iMac <imac@Regge.local>
2020-04-16 15:33:09 +0800

    rust grpc service

---
+++ src/client.rs
@@ -0,0 +1,23 @@
+use hello_world::greeter_client::GreeterClient;
+use hello_world::HelloRequest;
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+   
+    let mut client = GreeterClient::connect("http://127.0.0.1:50051").await?;
+
+    let request:tonic::Request<HelloRequest> = tonic::Request::new(HelloRequest {
+        name: "rust client".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("response={:?}", response);
+
+    Ok(())
+}
